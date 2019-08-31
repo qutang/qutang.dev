77a0d6797f76edc8f74911c28424a521884a4af1
Qu Tang <qutang@Qus-MacBook-Pro.local>
2019-08-31 15:51:24 -0400

    Optimize series label display and archive link
    Close #9

--- components/blog-item.jsx
+++ components/blog-item.jsx
@@ -1,20 +1,31 @@
-import Link from './link'
-import moment from 'moment'
+import Link from "./link";
+import moment from "moment";
 
 export default ({ post }) => (
-    <section className='blog-item-container'>
-        <header className='blog-item-header-container'>
-            <h2>
-                <Link to={post.path}>{post.meta.title}</Link>
-            </h2>
-        </header>
-        <section className='blog-item-description-container'>
-            <p className='blog-item-first-letter'>{post.meta.excerpt.slice(0, 1)}</p>
-            <p className='blog-item-description'>{post.meta.excerpt.slice(1)}</p>
-        </section>
-        <footer className='blog-item-footer-container'>
-            <span className='blog-item-series'><a href={"/blog/series#" + post.meta.series || ""}>{post.meta.series || ""}</a></span>{" | "}
-            <time className='blog-item-date'><a href={"/blog/archive#" + moment(post.meta.date).format("YYYY")}>{moment(post.meta.date).format("MMM DD, YYYY")}</a></time>
-        </footer>
+  <section className="blog-item-container">
+    <header className="blog-item-header-container">
+      <h2>
+        <Link to={post.path}>{post.meta.title}</Link>
+      </h2>
+    </header>
+    <section className="blog-item-description-container">
+      <p className="blog-item-first-letter">{post.meta.excerpt.slice(0, 1)}</p>
+      <p className="blog-item-description">{post.meta.excerpt.slice(1)}</p>
     </section>
-)
+    <footer className="blog-item-footer-container">
+      {post.meta.series && (
+        <span className="blog-item-series">
+          <a href={"/blog/series#" + post.meta.series || ""}>
+            {post.meta.series || ""}
+          </a>{" "}
+          {" | "}
+        </span>
+      )}
+      <time className="blog-item-date">
+        <a href={"/blog/archive#" + moment(post.meta.date).format("YYYY")}>
+          {moment(post.meta.date).format("MMM DD, YYYY")}
+        </a>
+      </time>
+    </footer>
+  </section>
+);
--- layouts/post.jsx
+++ layouts/post.jsx
@@ -1,23 +1,37 @@
-import TwoColumnLayout from './two-column'
-import moment from 'moment'
+import TwoColumnLayout from "./two-column";
+import moment from "moment";
+
+import Link from "../components/link";
 
 export default ({ meta, ...props }) => (
-    <TwoColumnLayout {...props} title={meta.title} css="../css/post.css" pageType='post'>
-        <header><h2>{meta.title}</h2></header>
-        <section className='post-meta-container'>
-            <span className='post-meta-series'>
-                <a href={"/blog/series#" + meta.series || ""}>{meta.series || ""}</a>
-            </span>{" | "}
-            <time className='post-meta-date'>
-                {moment(meta.date).format('MMM DD, YYYY')}
-            </time>
-        </section>
-        <article>
-            {props.children}
-        </article>
-        <section class='post-comment-container'>
-            <h2 id='comments'>Comments</h2>
-            <div id='gitalk-container'></div>
-        </section>
-    </TwoColumnLayout>
+  <TwoColumnLayout
+    {...props}
+    title={meta.title}
+    css="../css/post.css"
+    pageType="post"
+  >
+    <header>
+      <h2>{meta.title}</h2>
+    </header>
+    <section className="post-meta-container">
+      {meta.series && (
+        <span className="post-meta-series">
+          <Link to={"/blog/series#" + meta.series || ""}>
+            {meta.series || ""}
+          </Link>
+          {" | "}
+        </span>
+      )}
+      <time className="post-meta-date">
+        <Link to={"/blog/archive#" + moment(meta.date).format("YYYY")}>
+          {moment(meta.date).format("MMM DD, YYYY")}
+        </Link>
+      </time>
+    </section>
+    <article>{props.children}</article>
+    <section class="post-comment-container">
+      <h2 id="comments">Comments</h2>
+      <div id="gitalk-container"></div>
+    </section>
+  </TwoColumnLayout>
 );
