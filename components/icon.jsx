df49181c8ca406ec0d0eeb5dbcfec5a1f1785539
qutang <tqshelly@gmail.com>
2019-09-02 12:13:03 -0400

    Close #11

---
+++ components/icon.jsx
@@ -0,0 +1,44 @@
+import Blog from '../content/icons/blog.svg'
+import Charge from '../content/icons/charge.js.svg'
+import Comment from '../content/icons/comment.svg'
+import Github from '../content/icons/github.svg'
+import Home from '../content/icons/home.svg'
+import LinkedIn from '../content/icons/linkedin.svg'
+import Lost from '../content/icons/lost.svg'
+import Medium from '../content/icons/medium.svg'
+import Plus from '../content/icons/plus.svg'
+import Rss from '../content/icons/rss.svg'
+import Telegram from '../content/icons/telegram.svg'
+import User from '../content/icons/user.svg'
+import Wechat from '../content/icons/wechat.svg'
+
+export default (type) => {
+    switch(type){
+        case 'blog':
+            return Blog
+        case 'chargejs':
+            return Charge
+        case 'comment':
+            return Comment
+        case 'github':
+            return Github
+        case 'home':
+            return Home
+        case 'linkedin':
+            return LinkedIn
+        case 'lost':
+            return Lost
+        case 'medium':
+            return Medium
+        case 'plus':
+            return Plus
+        case 'rss':
+            return Rss
+        case 'telegram':
+            return Telegram
+        case 'user':
+            return User
+        case 'wechat':
+            return Wechat
+    }
+}
--- components/shortcut-item.jsx
+++ components/shortcut-item.jsx
@@ -1,10 +1,16 @@
 import Link from "./link";
+import icons from './icon';
 
-export default props => (
-  <div className="shortcut-item">
-    <Link to={props.to} className="color-scheme-light">
-      <img src={"../icons/" + props.iconType + ".svg"} alt="" />
-      <span>{props.text}</span>
-    </Link>
-  </div>
-);
+
+export default props => {
+  const Icon = icons(props.iconType)
+  return (
+    <div className="shortcut-item">
+      <Link to={props.to} className="color-scheme-light">
+        <Icon />
+        {/* <img src={"../icons/" + props.iconType + ".svg"} alt="" /> */}
+        <span>{props.text}</span>
+      </Link>
+    </div>
+  )
+};
