ef38489c57417dcf0457f02550fd395328d40448
Qu Tang <qutang@Qus-MacBook-Pro.local>
2019-09-10 20:53:23 -0400

    Redesign sidebar

--- components/side.jsx
+++ components/side.jsx
@@ -6,10 +6,12 @@ import Copyright from "./copyright";
 export default ({ data, pageType }) => {
   return (
     <aside className="side-container">
-      <Avatar avatar={data.site.avatar} />
-      <Shortcuts shortcuts={data.shortcuts} pageType={pageType} />
-      {pageType === "post" && <Copyright data={data} />}
-      <Footer data={data} />
+      <div className="side-inner-container">
+        <Avatar avatar={data.site.avatar} />
+        <Shortcuts shortcuts={data.shortcuts} pageType={pageType} />
+        {pageType === "post" && <Copyright data={data} />}
+        <Footer data={data} />
+      </div>
     </aside>
   );
 };
--- layouts/two-column.jsx
+++ layouts/two-column.jsx
@@ -1,19 +1,23 @@
-import Head from '../components/head'
-import Main from '../components/main'
-import Side from '../components/side'
-import MenuButton from '../components/menu-button'
+import Head from "../components/head";
+import Main from "../components/main";
+import Side from "../components/side";
+import RightSide from "../components/right-side";
+import MenuButton from "../components/menu-button";
 
 export default ({ data, children, css, title, pageType, environment }) => (
-    <html lang="en">
-        <Head title={title} css={css} siteName={data.site.title} pageType={pageType} />
-        <body>
-            <div className='layout-container'>
-                <Side data={data} pageType={pageType}></Side>
-                <Main data={data}>
-                    {children}
-                </Main>
-                <MenuButton data={data} />
-            </div>
-        </body>
-    </html>
-)
+  <html lang="en">
+    <Head
+      title={title}
+      css={css}
+      siteName={data.site.title}
+      pageType={pageType}
+    />
+    <body>
+      <div className="layout-container">
+        <Side data={data} pageType={pageType}></Side>
+        <Main data={data}>{children}</Main>
+        <MenuButton data={data} />
+      </div>
+    </body>
+  </html>
+);
