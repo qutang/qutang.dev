fe1fbf21c19d7e8ed3209a2c6162eee970a94b20
qutang <tqshelly@gmail.com>
2019-10-04 17:20:59 -0400

    Enable Netlify CMS
    Close #48

---
+++ content/admin.html.jsx
@@ -0,0 +1,14 @@
+export default props => {
+    return (
+        <html>
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Content Manager</title>
+            </head>
+            <body>
+                <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
+            </body>
+        </html>
+    )
+}
