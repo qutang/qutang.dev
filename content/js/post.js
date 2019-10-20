510304927b153fb7f308dab6c1809ba9e83cf500
qutang <tqshelly@gmail.com>
2019-10-19 22:52:04 -0400

    Refine toc style
    1. Update all articles to use markdown
    2. Refine toc style to be responsive

--- content/js/post.js
+++ content/js/post.js
@@ -17,9 +17,9 @@ document.addEventListener("DOMContentLoaded", function () {
         // Where to render the table of contents.
         tocSelector: '.side-toc',
         // Where to grab the headings to build the table of contents.
-        contentSelector: 'article',
+        contentSelector: '.main-inner-container',
         // Which headings to grab inside of the contentSelector element.
-        headingSelector: 'h1, h2, h3',
+        headingSelector: 'h2, h3',
         // For headings inside relative or absolute positioned containers within content.
         hasInnerContainers: true,
     })
