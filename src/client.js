3774cc8e3e0b9aeed65deebeaeddc833584a004b
qutang <tqshelly@gmail.com>
2020-02-22 17:13:55 -0500

    fix: Render mermaid when switching language

--- src/client.js
+++ src/client.js
@@ -1,4 +1,5 @@
 import * as sapper from "@sapper/app";
+
 sapper
   .start({
     target: document.querySelector("#sapper")
@@ -7,11 +8,16 @@ sapper
     console.log("client-side app has started");
     mermaid.init(undefined, document.querySelectorAll(".mermaid"));
     var oldHref = document.location.href;
+    var oldLang = document.querySelector('.language').textContent;
     var bodyList = document.querySelector("body"),
-      observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
+      observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
           if (oldHref != document.location.href) {
             oldHref = document.location.href;
+            mermaid.init(undefined, document.querySelectorAll(".mermaid"));
+          }
+          if (oldLang != document.querySelector('.language').textContent) {
+            oldLang = document.querySelector('.language').textContent;
             mermaid.init(undefined, document.querySelectorAll(".mermaid"));
           }
         });
