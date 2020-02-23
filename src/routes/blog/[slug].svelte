f94e44420b74ff0a7b8eaa5f6fac0026fdc01028
qutang <tqshelly@gmail.com>
2020-02-23 12:47:08 -0500

    feat: Add creative commons license to posts

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -22,6 +22,7 @@
   import zh from 'moment/locale/zh-cn';
 
   let edit_text = undefined;
+  let license_text = undefined;
   let on_colab = post.src.includes('colab');
   let edit_where = on_colab ? "Colab" : "Github";
   if ($lang == 'cn') {
@@ -55,6 +56,15 @@
   .series a{
     text-decoration: none;
   }
+
+  .license {
+    padding: 0.5em;
+    background: coral;
+    filter: brightness(150%);
+    border-radius: 5px;
+    color: black;
+    font-weight: normal;
+  }
 </style>
 
 <svelte:head>
@@ -66,6 +76,15 @@
 <h1>{post.title}</h1>
 
 <p><span class='series'><a href="/blog/series/{post.series}">{series[post.series][$lang]}</a></span> {moment(post.date).locale($lang == 'cn' ? 'zh-cn' : 'en').format("LL")} <a href={post.src} rel='noopener' target="_blank" style='float:right; margin-top: 0'>{edit_text}</a></p>
+
+<p class='license'>
+  {#if $lang == 'cn'}
+  <a rel="license" target='_blank' href="http://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a> 本作品采用<a rel="license" target='_blank' href="http://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。
+  {:else}
+  <a rel="license" target='_blank' href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a> 
+  This work is licensed under a <a rel="license" target='_blank' href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+  {/if}
+</p>
 
   {@html post.html}
 </div>
