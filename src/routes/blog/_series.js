a620ac47f3dacb82ff8cb465115285b90ee3f2e5
qutang <qu.tang@outlook.com>
2020-08-28 20:51:45 -0400

    fix: Change the name of series

--- src/routes/blog/_series.js
+++ src/routes/blog/_series.js
@@ -2,6 +2,6 @@ export default {
   ml: { cn: "哆啦A梦", en: "Machine learning", emoji: "🤖" },
   programming: { cn: "鬼话桃符", en: "Programming", emoji: "🎰" },
   voyage: { cn: "航海日志", en: "Notes", emoji: "🚀" },
-  stats: { cn: "统计学笔记", en: "Statistics", emoji: "📈" },
-  survivalkit: { cn: "北美博士生生存指南", en: "Live as a Ph.D.", emoji: "🎓" }
+  stats: { cn: "女士品茶", en: "Statistics", emoji: "📈" },
+  survivalkit: { cn: "北美博士生生存指南", en: "Live as a Ph.D.", emoji: "🎓" },
 };
