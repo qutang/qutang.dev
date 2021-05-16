f378557c25a46f817b0e467ee69d388fa0495651
qutang <qu.tang@outlook.com>
2021-05-16 16:02:22 -0400

    feat: Add 404 page
    Close #293

---
+++ src/routes/404.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Not found</title>
+</svelte:head>
+
+<div>
+    <h1>404</h1>
+</div>
+
+
+<style>
+    h1 {
+        text-align: center;
+        margin: 0 auto;
+        margin-top: 100px;
+        font-size: 10em;
+        font-family: 'Lora';
+    }
+</style>
---
+++ src/routes/__error.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		};
+	}
+</script>
+
+<script>
+	export let title;
+</script>
+
+<div>
+    <h1>404</h1>
+</div>
+
+
+<style>
+    h1 {
+        text-align: center;
+        margin: 0 auto;
+        margin-top: 100px;
+        font-size: 10em;
+        font-family: 'Lora';
+    }
+</style>
